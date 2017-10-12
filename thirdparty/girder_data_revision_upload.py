951e35f8d2d7a307fe43921852dc9d2e5a238872
Eric Cousineau <eric.cousineau@tri.global>
2017-10-12 15:53:16 -0400

    Automate sha512 usage.

--- thirdparty/girder_data_revision_upload.py
+++ thirdparty/girder_data_revision_upload.py
@@ -6,6 +6,7 @@
 import girder_client
 import json
 import os
+import subprocess
 import sys
 import textwrap
 
@@ -22,8 +23,11 @@ def upload(server, api_key, folder_id, project_root, filepath):
     print("folder_id ..........: %s" % folder_id)
     print("filepath ...........: %s" % filepath)
     print("item_name ..........: %s" % item_name)
-    print("project_root .......: %s" % folder_id)
+    print("project_root .......: %s" % project_root)
     print("versioned_filepath .: %s" % versioned_filepath)
+
+    sha = _subshell(['sha512sum', filepath]).split(' ')[0]
+    print("sha512 .............: %s" % sha)
 
     gc = girder_client.GirderClient(apiUrl=api_url)
     gc.authenticate(apiKey=api_key)
@@ -32,7 +36,16 @@ def upload(server, api_key, folder_id, project_root, filepath):
 
     size = os.stat(filepath).st_size
     with open(filepath, 'rb') as fd:
+        print("Uploading: {}".format(filepath))
         gc.uploadFile(folder_id, fd, name=item_name, size=size, parentType='folder', reference=ref) 
+
+    # Write SHA512
+    sha_file = filepath + '.sha512'
+    with open(sha_file, 'w') as fd:
+        print("Updating sha file: {}".format(sha_file))
+        fd.write(sha)
+
+    print("[ Done ]")
 
 
 def display_usage():
@@ -76,10 +89,29 @@ def main():
         display_error("'/path/to/filename' is not specified")
         sys.exit(1)
 
+    # TODO(eric.cousineau): Use `bazel info workspace`?
     project_root = os.path.join(os.path.dirname(__file__), "..")
     filepath = sys.argv[1]
 
     upload(server, api_key, folder_id, project_root, filepath)
+
+
+def _subshell(cmd, suppress_error=False, strip=True):
+    try:
+        if isinstance(cmd, list):
+            output = subprocess.check_output(cmd)
+        else:
+            assert isinstance(cmd, str)
+            output = subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        if suppress_error:
+            return None
+        else:
+            raise e
+    if strip:
+        return output.strip()
+    else:
+        return output
 
 
 if __name__ == '__main__':
