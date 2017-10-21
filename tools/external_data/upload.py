12162acb03b4368cf150238e53692b3a81e9f686
Eric Cousineau <eric.cousineau@tri.global>
2017-10-20 21:04:59 -0400

    Clean up aesthetics.

--- tools/external_data/upload.py
+++ tools/external_data/upload.py
@@ -20,23 +20,23 @@ def upload(conf, filepath, do_cache):
     filepath = os.path.abspath(filepath)
     item_name = "%s %s" % (os.path.basename(filepath), datetime.utcnow().isoformat())
 
+    sha = util.subshell(['sha512sum', filepath]).split(' ')[0]
     print("api_url ............: %s" % conf.api_url)
     print("folder_id ..........: %s" % conf.folder_id)
     print("filepath ...........: %s" % filepath)
-    print("item_name ..........: %s" % item_name)
-
-    if conf.project_root:
-        versioned_filepath = os.path.relpath(filepath, conf.project_root)
-        print("project_root .......: %s" % project_root)
-        print("versioned_filepath .: %s" % versioned_filepath)
-        ref = json.dumps({'versionedFilePath': versioned_filepath})
-    else:
-        ref = None
-
-    sha = util.subshell(['sha512sum', filepath]).split(' ')[0]
     print("sha512 .............: %s" % sha)
 
     if not util.is_sha_uploaded(conf, sha):
+        print("item_name ..........: %s" % item_name)
+
+        if conf.project_root:
+            versioned_filepath = os.path.relpath(filepath, conf.project_root)
+            print("project_root .......: %s" % project_root)
+            print("versioned_filepath .: %s" % versioned_filepath)
+            ref = json.dumps({'versionedFilePath': versioned_filepath})
+        else:
+            ref = None
+
         gc = girder_client.GirderClient(apiUrl=conf.api_url)
         gc.authenticate(apiKey=conf.api_key)
 
@@ -54,25 +54,28 @@ def upload(conf, filepath, do_cache):
         fd.write(sha)
 
     # Place copy in cache.
+    # @note This is purposely simpler than `download`s caching logic, so that developers can easily
+    # test the round-trip experience without prematurely caching.
+    # This will overwrite the cached, even if it already exists.
     if do_cache:
         cache_path = util.get_sha_cache_path(conf, sha, create_dir=True)
         print("Cache path: {}".format(cache_path))
         with util.FileWriteLock(cache_path):
-            subshell(['cp', filepath, cache_path])
+            util.subshell(['cp', filepath, cache_path])
             # Make cache file read-only.
-            subshell(['chmod', '-w', cache_path])
+            util.subshell(['chmod', '-w', cache_path])
 
     print("[ Done ]")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--no_cache', action='store_true')
+    parser.add_argument('--do_cache', action='store_true')
     parser.add_argument('filepath', type=str)
     args = parser.parse_args()
 
     conf = util.get_all_conf(do_auth=True)
-    upload(conf, args.filepath, not args.no_cache)
+    upload(conf, args.filepath, args.do_cache)
 
 
 if __name__ == '__main__':
