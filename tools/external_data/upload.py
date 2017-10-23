4e7be2cb4283c7913809f8eecf3728971f8a3984
Eric Cousineau <eric.cousineau@tri.global>
2017-10-23 13:35:11 -0400

    Simplify uploading logic

--- tools/external_data/upload.py
+++ tools/external_data/upload.py
@@ -21,11 +21,17 @@ from external_data import util
 SHA_SUFFIX = '.sha512'
 
 def upload(conf, filepath, do_cache):
+    if not os.path.isabs(filepath):
+        raise RuntimeError("Must supply absolute path: {}".format(filepath))
     filepath = os.path.abspath(filepath)
     if filepath.endswith(SHA_SUFFIX):
         filepath_guess = filepath[:-len(SHA_SUFFIX)]
         raise RuntimeError("Input file is a SHA file. Did you mean to upload '{}' instead?".format(filepath_guess))
     item_name = "%s %s" % (os.path.basename(filepath), datetime.utcnow().isoformat())
+
+    versioned_filepath = os.path.relpath(filepath, conf.project_root)
+    if versioned_filepath.startswith('..'):
+        raise RuntimeError("File to upload, '{}', must be under '{}'".format(filepath, conf.project_root))
 
     sha = util.subshell(['sha512sum', filepath]).split(' ')[0]
     print("api_url ............: %s" % conf.api_url)
@@ -35,17 +41,9 @@ def upload(conf, filepath, do_cache):
 
     if not util.is_sha_uploaded(conf, sha):
         print("item_name ..........: %s" % item_name)
-
-        if conf.project_root:
-            # TODO: Assert that this file is contained under project_root.
-            versioned_filepath = os.path.relpath(filepath, conf.project_root)
-            if versioned_filepath.startswith('..'):
-                raise RuntimeError("File to upload, '{}', must be under '{}'".format(filepath, conf.project_root))
-            print("project_root .......: %s" % conf.project_root)
-            print("versioned_filepath .: %s" % versioned_filepath)
-            ref = json.dumps({'versionedFilePath': versioned_filepath})
-        else:
-            ref = None
+        print("project_root .......: %s" % conf.project_root)
+        print("versioned_filepath .: %s" % versioned_filepath)
+        ref = json.dumps({'versionedFilePath': versioned_filepath})
 
         gc = girder_client.GirderClient(apiUrl=conf.api_url)
         gc.authenticate(apiKey=conf.api_key)
@@ -55,6 +53,7 @@ def upload(conf, filepath, do_cache):
             print("Uploading: {}".format(filepath))
             gc.uploadFile(conf.folder_id, fd, name=item_name, size=size, parentType='folder', reference=ref)
     else:
+        # TODO(eric.cousineau): Check and ensure 
         print("File already uploaded")
 
     # Write SHA512
